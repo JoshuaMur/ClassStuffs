5c3a65d94ee5ef5a4c6b329cb23591213880edbf
TheJoshVerse <joshuamurangabauer@gmail.com>
2024-02-26 09:51:13 +0300

    Forgotten Updates

--- oopspot.py
+++ oopspot.py
@@ -1,3 +1,12 @@
 class TheBiginning:
     def __init__(self):
         return "I can run in the air"
+
+    def a_josh_method(self, med, fast):
+        self.med = med
+        self.fast = fast
+        pass
+
+    def another_method(self):
+        pass
+
